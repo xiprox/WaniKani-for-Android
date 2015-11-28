7b013c3de5b5697ebb25ebb7c6b5a0d9d80bf6e7
Gustorn <gustorn@gmail.com>
2015-11-28 23:39:46 +0100

    Improved performance on progress details
    Introduced font caching to avoid the repeated creation of the normal and custom fonts. This resolves the visible pause on the progress screen when there are a lot of items in the remaining queue.

--- WaniKani/src/tr/xip/wanikani/utils/Fonts.java
+++ WaniKani/src/tr/xip/wanikani/utils/Fonts.java
@@ -9,11 +9,16 @@ import tr.xip.wanikani.managers.PrefManager;
  * Created by xihsa_000 on 3/23/14.
  */
 public class Fonts {
+    private static Typeface custom = null;
+    private static Typeface normal = Typeface.create("sans-serif", Typeface.NORMAL);
 
     public Typeface getKanjiFont(Context context) {
-        if (new PrefManager(context).isUseCustomFonts())
-            return Typeface.createFromAsset(context.getAssets(), "fonts/MTLmr3m.ttf");
-        else
-            return Typeface.create("sans-serif", Typeface.NORMAL);
+        if (new PrefManager(context).isUseCustomFonts()) {
+            if (custom == null)
+                custom = Typeface.createFromAsset(context.getAssets(), "fonts/MTLmr3m.ttf");
+            return custom;
+        } else {
+            return normal;
+        }
     }
 }
