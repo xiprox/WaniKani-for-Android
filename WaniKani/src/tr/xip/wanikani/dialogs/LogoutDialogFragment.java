72c9dc321a2e7a0d3062abcd58464357a7c7d53a
veyndan <veyndan89@gmail.com>
2016-11-18 01:57:58 +0000

    Use AlertDialog for Logout dialog

--- WaniKani/src/tr/xip/wanikani/dialogs/LogoutDialogFragment.java
+++ WaniKani/src/tr/xip/wanikani/dialogs/LogoutDialogFragment.java
@@ -1,18 +1,14 @@
 package tr.xip.wanikani.dialogs;
 
 import android.app.Dialog;
-import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.Window;
-import android.widget.Button;
+import android.support.v7.app.AlertDialog;
 
-import tr.xip.wanikani.app.activity.FirstTimeActivity;
 import tr.xip.wanikani.R;
+import tr.xip.wanikani.app.activity.FirstTimeActivity;
 import tr.xip.wanikani.managers.PrefManager;
 
 /**
@@ -20,47 +16,27 @@ import tr.xip.wanikani.managers.PrefManager;
  */
 public class LogoutDialogFragment extends DialogFragment {
 
-    Context context;
-    PrefManager prefMan;
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        context = getActivity();
-        prefMan = new PrefManager(context);
-    }
-
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        Dialog dialog = super.onCreateDialog(savedInstanceState);
-        dialog.getWindow().requestFeature(Window.FEATURE_NO_TITLE);
-        return dialog;
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.dialog_logout, null);
-
-        Button mOk = (Button) view.findViewById(R.id.button1);
-        Button mCancel = (Button) view.findViewById(R.id.button2);
-
-        mOk.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                prefMan.logout();
-                startActivity(new Intent(getActivity(), FirstTimeActivity.class));
-                getActivity().finish();
-                dismiss();
-            }
-        });
-
-        mCancel.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                dismiss();
-            }
-        });
-
-        return view;
+        return new AlertDialog.Builder(getActivity())
+                .setTitle(R.string.dialog_logout_title)
+                .setMessage(R.string.dialog_logout_message)
+                .setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        PrefManager prefMan = new PrefManager(getActivity());
+                        prefMan.logout();
+                        startActivity(new Intent(getActivity(), FirstTimeActivity.class));
+                        getActivity().finish();
+                        dismiss();
+                    }
+                })
+                .setNegativeButton(R.string.no, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dismiss();
+                    }
+                })
+                .create();
     }
 }
