aebcd6e1f7247e3467780d3dd262eedbb2839c35
veyndan <veyndan89@gmail.com>
2016-11-18 01:50:45 +0000

    Use AlertDialog for OpenSourceLicenses dialog

--- WaniKani/src/tr/xip/wanikani/dialogs/OpenSourceLicensesDialogFragment.java
+++ WaniKani/src/tr/xip/wanikani/dialogs/OpenSourceLicensesDialogFragment.java
@@ -1,15 +1,12 @@
 package tr.xip.wanikani.dialogs;
 
 import android.app.Dialog;
-import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.Window;
 import android.webkit.WebView;
-import android.widget.Button;
 
 import tr.xip.wanikani.R;
 
@@ -18,36 +15,23 @@ import tr.xip.wanikani.R;
  */
 public class OpenSourceLicensesDialogFragment extends DialogFragment {
 
-    final String licensesUrl = "file:///android_asset/licenses.html";
-    Context context;
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        context = getActivity();
-    }
+    private static final String LICENSES_URL = "file:///android_asset/licenses.html";
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        Dialog dialog = super.onCreateDialog(savedInstanceState);
-        dialog.getWindow().requestFeature(Window.FEATURE_NO_TITLE);
-        return dialog;
-    }
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        WebView webView = (WebView) inflater.inflate(R.layout.dialog_open_source_licenses, null);
+        webView.loadUrl(LICENSES_URL);
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.dialog_open_source_licenses, null);
-
-        Button mOk = (Button) view.findViewById(R.id.button1);
-        mOk.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                dismiss();
-            }
-        });
-
-        WebView mWebview = (WebView) view.findViewById(R.id.webview);
-        mWebview.loadUrl(licensesUrl);
-        return view;
+        return new AlertDialog.Builder(getActivity())
+                .setTitle(R.string.pref_title_open_source_licences)
+                .setView(webView)
+                .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dismiss();
+                    }
+                })
+                .create();
     }
 }
