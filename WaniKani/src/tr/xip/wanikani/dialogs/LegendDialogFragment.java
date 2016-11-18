8fa6e23f058207ae015d0000d3ae3b8661f6bc06
veyndan <veyndan89@gmail.com>
2016-11-18 02:12:09 +0000

    Use AlertDialog for Legend dialog

--- WaniKani/src/tr/xip/wanikani/dialogs/LegendDialogFragment.java
+++ WaniKani/src/tr/xip/wanikani/dialogs/LegendDialogFragment.java
@@ -1,14 +1,10 @@
 package tr.xip.wanikani.dialogs;
 
 import android.app.Dialog;
-import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.Window;
-import android.widget.Button;
+import android.support.v7.app.AlertDialog;
 
 import tr.xip.wanikani.R;
 import tr.xip.wanikani.managers.PrefManager;
@@ -18,35 +14,18 @@ import tr.xip.wanikani.managers.PrefManager;
  */
 public class LegendDialogFragment extends DialogFragment {
 
-    Context context;
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        context = getActivity();
-    }
-
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        Dialog dialog = super.onCreateDialog(savedInstanceState);
-        dialog.getWindow().requestFeature(Window.FEATURE_NO_TITLE);
-        return dialog;
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.dialog_legend, null);
-
-        Button mOk = (Button) view.findViewById(R.id.button1);
-
-        mOk.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                dismiss();
-                new PrefManager(context).setLegendLearned(true);
-            }
-        });
-
-        return view;
+        return new AlertDialog.Builder(getActivity())
+                .setTitle(R.string.content_radicals_legend)
+                .setView(R.layout.dialog_legend)
+                .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dismiss();
+                        new PrefManager(getActivity()).setLegendLearned(true);
+                    }
+                })
+                .create();
     }
 }
