3e8596f730452d34757d0b5d969a99cf3ce9d7e8
İhsan Işık <x.ihsan@gmail.com>
2017-01-05 13:02:18 +0100

    Don't enable single mode by default
    Closes #37

--- WaniKani/src/tr/xip/wanikani/app/activity/WebReviewActivity.java
+++ WaniKani/src/tr/xip/wanikani/app/activity/WebReviewActivity.java
@@ -652,7 +652,7 @@ public class WebReviewActivity extends ActionBarActivity {
     private Button singleb;
 
     /** Single mode is on? */
-    private boolean single = true;  // Added by @Aralox. Default: single mode selected.
+    private boolean single = false;  // Added by @Aralox. Default: single mode selected.
 
     /** Shall we download a file? */
     private boolean download;
