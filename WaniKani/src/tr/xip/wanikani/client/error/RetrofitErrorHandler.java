39973433f0bcde9244e2e7c318466c1fc55c65c4
Ihsan Isik <x.ihsan@gmail.com>
2017-02-11 19:01:41 +0100

    Hack that error handler
    ugh

--- WaniKani/src/tr/xip/wanikani/client/error/RetrofitErrorHandler.java
+++ WaniKani/src/tr/xip/wanikani/client/error/RetrofitErrorHandler.java
@@ -2,6 +2,9 @@ package tr.xip.wanikani.client.error;
 
 import android.content.Intent;
 import android.support.v4.content.LocalBroadcastManager;
+
+import java.lang.reflect.Field;
+
 import tr.xip.wanikani.app.App;
 import tr.xip.wanikani.content.receiver.BroadcastIntents;
 
@@ -9,14 +12,24 @@ public class RetrofitErrorHandler {
     public static void handleError(Throwable throwable) {
 
         if (throwable != null) {
-            String cause = throwable.getCause().toString();
-            if (cause.contains("GaiException") || cause.contains("UnknownHostException")) {
-                Intent intent = new Intent(BroadcastIntents.RETROFIT_ERROR_CONNECTION());
-                LocalBroadcastManager.getInstance(App.getContext()).sendBroadcast(intent);
+            try {
+                // Ghhhhh!
+                Field f = Throwable.class.getDeclaredField("cause");
+                f.setAccessible(true);
+                String message = f.get(throwable).toString();
+
+                if (message.contains("GaiException") || message.contains("UnknownHostException")) {
+                    Intent intent = new Intent(BroadcastIntents.RETROFIT_ERROR_CONNECTION());
+                    LocalBroadcastManager.getInstance(App.getContext()).sendBroadcast(intent);
+                } else {
+                    Intent intent = new Intent(BroadcastIntents.RETROFIT_ERROR_UNKNOWN());
+                    LocalBroadcastManager.getInstance(App.getContext()).sendBroadcast(intent);
+                }
+            } catch (NoSuchFieldException e) {
+                e.printStackTrace();
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
             }
-        } else {
-            Intent intent = new Intent(BroadcastIntents.RETROFIT_ERROR_UNKNOWN());
-            LocalBroadcastManager.getInstance(App.getContext()).sendBroadcast(intent);
         }
     }
 }
