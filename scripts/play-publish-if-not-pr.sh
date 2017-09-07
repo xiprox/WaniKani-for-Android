8f68cd3630b8371e1d06cff8b8debcf6c2d506e9
İhsan Işık <x.ihsan@gmail.com>
2017-09-07 11:33:14 +0400

    Fix PR Travis builds failing because of missing google-services.json

--- scripts/play-publish-if-not-pr.sh
+++ scripts/play-publish-if-not-pr.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 set -ev
+
+# Prepare env. Keeping this in another script didn't work. I tried enough (15 times/builds)
+cp WaniKani/google-services-dummy.json WaniKani/google-services.json
+
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ $TRAVIS_BRANCH == 'master' ]; then
   echo "Assembling and publishing release"
   ./gradlew assembleRelease publishApkRelease
