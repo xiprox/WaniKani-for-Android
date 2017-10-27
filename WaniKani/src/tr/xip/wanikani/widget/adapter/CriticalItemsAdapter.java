e5fe2bea5fa9116a74080977e1911d2b96e45695
Ihsan Isik <x.ihsan@gmail.com>
2017-10-27 15:08:41 +0200

    ugh

--- WaniKani/src/tr/xip/wanikani/widget/adapter/CriticalItemsAdapter.java
+++ WaniKani/src/tr/xip/wanikani/widget/adapter/CriticalItemsAdapter.java
@@ -48,42 +48,42 @@ public class CriticalItemsAdapter extends ArrayAdapter<CriticalItem> {
         if (v == null) {
             LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             v = inflater.inflate(R.layout.item_critical, null);
-
-            mItemType = v.findViewById(R.id.item_critical_type);
-            mItemCharacter = (TextView) v.findViewById(R.id.item_critical_character);
-            mItemCharacterImage = (ImageView) v.findViewById(R.id.item_critical_character_image);
-            mItemPercentage = (TextView) v.findViewById(R.id.item_critical_percentage);
-
-            mItemCharacter.setTypeface(typeface);
-
-            if (item.getType() == BaseItem.ItemType.RADICAL) {
-                mItemType.setBackgroundResource(R.drawable.oval_radical);
-            }
-
-            if (item.getType() == BaseItem.ItemType.KANJI) {
-                mItemType.setBackgroundResource(R.drawable.oval_kanji);
-            }
-
-            if (item.getType() == BaseItem.ItemType.VOCABULARY) {
-                mItemType.setBackgroundResource(R.drawable.oval_vocabulary);
-            }
-
-            if (item.getImage() == null) {
-                mItemCharacter.setVisibility(View.VISIBLE);
-                mItemCharacterImage.setVisibility(View.GONE);
-                mItemCharacter.setText(item.getCharacter());
-            } else {
-                mItemCharacter.setVisibility(View.GONE);
-                mItemCharacterImage.setVisibility(View.VISIBLE);
-                Picasso.with(context)
-                        .load(item.getImage())
-                        .into(mItemCharacterImage);
-                mItemCharacterImage.setColorFilter(context.getResources().getColor(R.color.text_gray), PorterDuff.Mode.SRC_ATOP);
-            }
-
-            mItemCharacter.setText(item.getCharacter());
-            mItemPercentage.setText(item.getPercentage() + "");
         }
+
+        mItemType = v.findViewById(R.id.item_critical_type);
+        mItemCharacter = (TextView) v.findViewById(R.id.item_critical_character);
+        mItemCharacterImage = (ImageView) v.findViewById(R.id.item_critical_character_image);
+        mItemPercentage = (TextView) v.findViewById(R.id.item_critical_percentage);
+
+        mItemCharacter.setTypeface(typeface);
+
+        if (item.getType() == BaseItem.ItemType.RADICAL) {
+            mItemType.setBackgroundResource(R.drawable.oval_radical);
+        }
+
+        if (item.getType() == BaseItem.ItemType.KANJI) {
+            mItemType.setBackgroundResource(R.drawable.oval_kanji);
+        }
+
+        if (item.getType() == BaseItem.ItemType.VOCABULARY) {
+            mItemType.setBackgroundResource(R.drawable.oval_vocabulary);
+        }
+
+        if (item.getImage() == null) {
+            mItemCharacter.setVisibility(View.VISIBLE);
+            mItemCharacterImage.setVisibility(View.GONE);
+            mItemCharacter.setText(item.getCharacter());
+        } else {
+            mItemCharacter.setVisibility(View.GONE);
+            mItemCharacterImage.setVisibility(View.VISIBLE);
+            Picasso.with(context)
+                    .load(item.getImage())
+                    .into(mItemCharacterImage);
+            mItemCharacterImage.setColorFilter(context.getResources().getColor(R.color.text_gray), PorterDuff.Mode.SRC_ATOP);
+        }
+
+        mItemCharacter.setText(item.getCharacter());
+        mItemPercentage.setText(item.getPercentage() + "");
 
         return v;
     }
